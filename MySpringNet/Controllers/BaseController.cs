ba98ae2de0ba54a4311ac4e5af670294c4d4bb75
ouyangyl <26492396@qq.com>
2015-09-29 17:52:45 +0800

    spring.net

---
+++ MySpringNet/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MySpringNet.Controllers
+{
+    public class BaseController
+    {
+        public int PageSize { get; set; }
+    }
+}
