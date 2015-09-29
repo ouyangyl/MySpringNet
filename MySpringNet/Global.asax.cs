2beff9ae33e158a7e7fbad6593ce1d37bff6eac1
ouyangyl <26492396@qq.com>
2015-09-29 11:24:18 +0800

    add springNet project

--- MySpringNet/Global.asax.cs
+++ MySpringNet/Global.asax.cs
@@ -6,13 +6,14 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Spring.Web.Mvc;
 
 namespace MySpringNet
 {
     // 注意: 有关启用 IIS6 或 IIS7 经典模式的说明，
     // 请访问 http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : SpringMvcApplication//System.Web.HttpApplication
     {
         protected void Application_Start()
         {
